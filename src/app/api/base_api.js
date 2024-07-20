8478a5842b1ab3a203c7aa00a2e3b16216361dba
syahmi mokhtar <syahmiez96@gmail.com>
2024-07-20 17:17:23 +0800

    edited

--- src/app/api/base_api.js
+++ src/app/api/base_api.js
@@ -6,7 +6,6 @@ if (!apiKey) {
   console.error('API key is not defined. Please set it in your .env.local file.');
 }
 
-
 const api = axios.create({
     baseURL:`https://v6.exchangerate-api.com/v6/${apiKey}`,
 });
